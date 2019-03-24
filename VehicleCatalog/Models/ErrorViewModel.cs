6db7d428174061591bc992f8afce44913c1fc019
001db001 <001db001@gmail.com>
2019-03-25 00:46:39 +0100

    Add project files.

---
+++ VehicleCatalog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VehicleCatalog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
